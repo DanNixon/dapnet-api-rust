3e29e3a9012198c1e0545b2b6261779efaa82580
Dan Nixon <dan@dan-nixon.com>
2024-06-01 16:00:55 +0100

    Add validation of message length in builder

--- src/types/calls.rs
+++ src/types/calls.rs
@@ -3,6 +3,7 @@ use derive_builder::Builder;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Builder)]
+#[builder(build_fn(validate = "Self::validate"))]
 pub struct OutgoingCall {
     /// Message text of the call
     pub(crate) text: String,
@@ -18,6 +19,21 @@ pub struct OutgoingCall {
     /// Flag indicating if this call was sent with high priority
     #[builder(default = "false")]
     pub(crate) emergency: bool,
+}
+
+impl OutgoingCallBuilder {
+    fn validate(&self) -> Result<(), String> {
+        match &self.text {
+            Some(text) => {
+                if text.len() > 80 {
+                    Err("Text must be 80 characters or less".to_string())
+                } else {
+                    Ok(())
+                }
+            }
+            None => Err("Text must be set".to_string()),
+        }
+    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -42,4 +58,50 @@ pub struct Call {
 
     /// Flag indicating if this call was sent with high priority
     pub emergency: bool,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn build_80_char() {
+        let text =
+            "01234567890123456789012345678901234567890123456789012345678901234567890123456789";
+        assert_eq!(text.len(), 80);
+
+        let call = OutgoingCallBuilder::default()
+            .recipients(vec!["m0nxn".to_string()])
+            .transmitter_groups(vec!["all".to_string()])
+            .text(text.to_string())
+            .build()
+            .unwrap();
+
+        assert_eq!(call.text, text);
+    }
+
+    #[test]
+    #[should_panic]
+    fn build_81_char() {
+        let text =
+            "012345678901234567890123456789012345678901234567890123456789012345678901234567890";
+        assert_eq!(text.len(), 81);
+
+        let _ = OutgoingCallBuilder::default()
+            .recipients(vec!["m0nxn".to_string()])
+            .transmitter_groups(vec!["all".to_string()])
+            .text(text.to_string())
+            .build()
+            .unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn build_no_text() {
+        let _ = OutgoingCallBuilder::default()
+            .recipients(vec!["m0nxn".to_string()])
+            .transmitter_groups(vec!["all".to_string()])
+            .build()
+            .unwrap();
+    }
 }
--- src/types/news.rs
+++ src/types/news.rs
@@ -3,6 +3,7 @@ use derive_builder::Builder;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Builder)]
+#[builder(build_fn(validate = "Self::validate"))]
 pub struct OutgoingNews {
     /// Name of the rubric to send to
     #[serde(rename = "rubricName")]
@@ -14,6 +15,21 @@ pub struct OutgoingNews {
     /// News position (1-10) for Skyper pagers
     #[builder(default = "1")]
     pub(crate) number: i8,
+}
+
+impl OutgoingNewsBuilder {
+    fn validate(&self) -> Result<(), String> {
+        match &self.text {
+            Some(text) => {
+                if text.len() > 80 {
+                    Err("Text must be 80 characters or less".to_string())
+                } else {
+                    Ok(())
+                }
+            }
+            None => Err("Text must be set".to_string()),
+        }
+    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -34,4 +50,47 @@ pub struct News {
     /// User who submitted the news
     #[serde(rename = "ownerName")]
     pub sender: String,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn build_80_char() {
+        let text =
+            "01234567890123456789012345678901234567890123456789012345678901234567890123456789";
+        assert_eq!(text.len(), 80);
+
+        let news = OutgoingNewsBuilder::default()
+            .rubric("test".to_string())
+            .text(text.to_string())
+            .build()
+            .unwrap();
+
+        assert_eq!(news.text, text);
+    }
+
+    #[test]
+    #[should_panic]
+    fn build_81_char() {
+        let text =
+            "012345678901234567890123456789012345678901234567890123456789012345678901234567890";
+        assert_eq!(text.len(), 81);
+
+        let _ = OutgoingNewsBuilder::default()
+            .rubric("test".to_string())
+            .text(text.to_string())
+            .build()
+            .unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn build_no_text() {
+        let _ = OutgoingNewsBuilder::default()
+            .rubric("test".to_string())
+            .build()
+            .unwrap();
+    }
 }
