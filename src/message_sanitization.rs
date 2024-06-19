f97ba35d260c68eedd7d24ae1ba541827acad3be
Dan Nixon <dan@dan-nixon.com>
2024-06-19 17:20:58 +0100

    Add sanitization of non-ASCII message characters

--- src/message_sanitization.rs
+++ src/message_sanitization.rs
@@ -6,10 +6,16 @@ pub struct MessageSanitizationOptions {
     /// Maximum message character length.
     ///
     /// DAPNET enforces an upper limit of 80, however shorter limits can be set if desired.
+    #[builder(default = "80")]
     max_length: usize,
 
     /// The string to include at the end of a truncated message.
+    #[builder(default = "\"...\".to_string()")]
     ellipses: String,
+
+    /// What to do with non-ASCII characters in the message.
+    #[builder(default = "MessageSanitizationNonAsciiPolicy::ReplaceWith('?')")]
+    non_ascii_policy: MessageSanitizationNonAsciiPolicy,
 }
 
 impl Default for MessageSanitizationOptions {
@@ -17,12 +23,36 @@ impl Default for MessageSanitizationOptions {
         Self {
             max_length: 80,
             ellipses: "...".to_string(),
+            non_ascii_policy: MessageSanitizationNonAsciiPolicy::ReplaceWith('?'),
         }
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum MessageSanitizationNonAsciiPolicy {
+    /// Don't attempt to change any non-ASCII characters and let DAPNET handle them.
+    DoNothing,
+
+    /// Just remove all non-ASCII characters.
+    Remove,
+
+    /// Replace each non-ASCII character with a specified char.
+    ReplaceWith(char),
+}
+
 /// Sanitize a message to ensure it is suitable for both POCSAG and DAPNET.
 pub fn sanitize_message(msg: String, options: &MessageSanitizationOptions) -> String {
+    // Remove non-ASCII characters
+    let msg = match &options.non_ascii_policy {
+        MessageSanitizationNonAsciiPolicy::DoNothing => msg,
+        MessageSanitizationNonAsciiPolicy::Remove => msg.chars().filter(|c| c.is_ascii()).collect(),
+        MessageSanitizationNonAsciiPolicy::ReplaceWith(replacement) => msg
+            .chars()
+            .map(|c| if c.is_ascii() { c } else { *replacement })
+            .collect(),
+    };
+
+    // Enforce maximum length
     if msg.len() > options.max_length {
         let trim_point = options.max_length.saturating_sub(options.ellipses.len());
         format!("{}{}", &msg[0..trim_point], options.ellipses)
@@ -36,6 +66,15 @@ mod test {
     use super::*;
 
     #[test]
+    fn options_builder_default_matches_options_default() {
+        let a = MessageSanitizationOptionsBuilder::default()
+            .build()
+            .unwrap();
+        let b = MessageSanitizationOptions::default();
+        assert_eq!(a, b);
+    }
+
+    #[test]
     fn build_options() {
         let options = MessageSanitizationOptionsBuilder::default()
             .ellipses("~~~".to_string())
@@ -47,7 +86,8 @@ mod test {
             options,
             MessageSanitizationOptions {
                 max_length: 50,
-                ellipses: "~~~".to_string()
+                ellipses: "~~~".to_string(),
+                non_ascii_policy: MessageSanitizationNonAsciiPolicy::ReplaceWith('?'),
             }
         );
     }
@@ -67,9 +107,66 @@ mod test {
             &MessageSanitizationOptions {
                 max_length: 20,
                 ellipses: "...".to_string(),
+                non_ascii_policy: MessageSanitizationNonAsciiPolicy::DoNothing,
             },
         );
         assert_eq!(sanitized, "This message is t...");
         assert_eq!(sanitized.len(), 20);
+    }
+
+    #[test]
+    fn message_with_non_ascii_do_nothing() {
+        let msg = "→ This message has non-ascii chars ❤, oh dear.".to_string();
+        let sanitized = sanitize_message(
+            msg.clone(),
+            &MessageSanitizationOptions {
+                max_length: 80,
+                ellipses: "...".to_string(),
+                non_ascii_policy: MessageSanitizationNonAsciiPolicy::DoNothing,
+            },
+        );
+        assert_eq!(sanitized, msg);
+    }
+
+    #[test]
+    fn message_with_non_ascii_remove() {
+        let msg = "→ This message has non-ascii chars ❤, oh dear.".to_string();
+        let sanitized = sanitize_message(
+            msg,
+            &MessageSanitizationOptions {
+                max_length: 80,
+                ellipses: "...".to_string(),
+                non_ascii_policy: MessageSanitizationNonAsciiPolicy::Remove,
+            },
+        );
+        assert_eq!(sanitized, " This message has non-ascii chars , oh dear.");
+    }
+
+    #[test]
+    fn message_with_non_ascii_replace_with() {
+        let msg = "→ This message has non-ascii chars ❤, oh dear.".to_string();
+        let sanitized = sanitize_message(
+            msg,
+            &MessageSanitizationOptions {
+                max_length: 80,
+                ellipses: "...".to_string(),
+                non_ascii_policy: MessageSanitizationNonAsciiPolicy::ReplaceWith('_'),
+            },
+        );
+        assert_eq!(sanitized, "_ This message has non-ascii chars _, oh dear.");
+    }
+
+    #[test]
+    fn non_ascii_removal_and_message_truncation_interoperate_correctly() {
+        let msg = "❤❤❤❤❤123456789".to_string();
+        let sanitized = sanitize_message(
+            msg,
+            &MessageSanitizationOptions {
+                max_length: 8,
+                ellipses: "...".to_string(),
+                non_ascii_policy: MessageSanitizationNonAsciiPolicy::Remove,
+            },
+        );
+        assert_eq!(sanitized, "12345...");
     }
 }
